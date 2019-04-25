21717e3c54a4c0d160d6dc8cdcfa2048f9826c81
Michael <darmodymichael@gmail.com>
2019-04-25 15:55:25 +0100

    Added feature that adds bookmarks to the data base from browser to database: across the whole stack.

--- spec/units/bookmark_spec.rb
+++ spec/units/bookmark_spec.rb
@@ -17,4 +17,12 @@ RSpec.describe Bookmark do
       expect(bookmark).to include("http://www.google.com")
     end
   end
+
+  describe '.create' do
+    it 'creates a new bookmark' do
+      Bookmark.create(url: 'http://www.testbookmark.com')
+
+      expect(Bookmark.all).to include 'http://www.testbookmark.com'
+    end
+  end
 end
